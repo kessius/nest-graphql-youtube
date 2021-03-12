d4451e2daad388a7c3be6b5dfbea803c0a602bd6
Angelo Gonçalves da Luz <angelogl@gmail.com>
2021-03-12 18:38:45 -0300

    :sparkles: Add remove feature to products

--- src/products/products.resolver.ts
+++ src/products/products.resolver.ts
@@ -9,7 +9,9 @@ export class ProductsResolver {
   constructor(private readonly productsService: ProductsService) {}
 
   @Mutation(() => Product)
-  createProduct(@Args('createProductInput') createProductInput: CreateProductInput) {
+  createProduct(
+    @Args('createProductInput') createProductInput: CreateProductInput,
+  ) {
     return this.productsService.create(createProductInput);
   }
 
@@ -33,6 +35,19 @@ export class ProductsResolver {
     );
   }
 
+  @Mutation(() => Product)
+  removeProduct(@Args('id', { type: () => Int }) id: number) {
+    return this.productsService.remove(id);
+  }
+
+  @Mutation(() => Product)
+  updateProduct(
+    @Args('updateProductInput') updateProductInput: UpdateProductInput,
+  ) {
+    return this.productsService.update(
+      updateProductInput.id,
+      updateProductInput,
+    );
   }
 
   @Mutation(() => Product)
